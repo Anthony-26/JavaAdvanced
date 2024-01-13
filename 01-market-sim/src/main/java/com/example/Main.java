e2244ab14319f824922fdcb844c8cfdfa524f334
Anthony <anthony.rousseau26@gmail.com>
2024-01-13 23:30:33 +0100

    Adding more features in console menu, updating AlphaClient.java fetch

--- 01-market-sim/src/main/java/com/example/Main.java
+++ 01-market-sim/src/main/java/com/example/Main.java
@@ -1,21 +1,12 @@
 package com.example;
 
-// import org.apache.logging.log4j.Logger;
-
-// import org.apache.logging.log4j.LogManager;
-
 import com.example.api.AlphaVantageClient;
-import com.example.model.StockData;
 import com.example.service.StockDataManager;
 import com.example.util.FormatStock;
 
 import java.util.Scanner;
 
 public class Main {
-
-    // private static final Logger logger =
-    // LogManager.getLogger(Main.class.getName());
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -26,12 +17,17 @@ public class Main {
         while (true) {
 
             System.out.println("""
-
+                    \n-----------------------------------------------\n
                     Menu :
                         0. Exit
-                        1. Fetch a daily based stock data
+                        1. Adding a stock in the database
                         2. Display the current stock list
-                            """);
+                        3. Get stock information
+
+                    -----------------------------------------------
+
+                    Select an action :""");
+
             String command = scanner.nextLine();
 
             if ("exit".equalsIgnoreCase(command) || "0".equalsIgnoreCase(command)) {
@@ -41,22 +37,43 @@ public class Main {
 
             switch (command) {
                 case "1":
-                    System.out.println("Enter stock ticker:");
+                    System.out.println("\nEnter stock ticker:");
                     String ticker = scanner.nextLine();
-                    String s = client.getDailyStockInformation(ticker);
-                    manager.addStockData(fs.FormatStockDaily(s));
+                    String s = ticker == null ? null : client.getDailyStockInformation(ticker);
+                    if (s == null) {
+                        System.out.println("\nError, " + ticker + " may not be an existig value and cannot be null.");
+                        break;
+                    }
+                    manager.addStockData(ticker.toUpperCase(), fs.FormatStockDaily(s));
                     System.out.println("Stock Data: " + s);
                     break;
 
                 case "2":
-                    System.out.println("Current list of stocks : " + manager.getRegisteredTickers());
+                    System.out
+                            .println("\nThe current list contains " + manager.getStockDataMap().size() + " stock(s).");
+                    System.out.println(manager.getRegisteredTickers());
+                    break;
+
+                case "3":
+                    System.out.println("\nEnter a known ticker : ");
+                    ticker = scanner.nextLine().toUpperCase();
+                    if(ticker == null){
+                        System.out.println("Ticker unknown.");
+                        break;
+                    }
+                    if(manager.getRegisteredTickers().contains(ticker)){
+                        System.out.println("\nData about " + ticker + " :");
+                        System.out.println(manager.getStockDataMap().get(ticker));
+                    }
+                    break;
 
                 default:
-                    System.out.println("Unknown command.");
+                    System.out.println("test");
                     break;
             }
 
         }
+        scanner.close();
 
     }
 }
---
+++ 01-market-sim/src/main/java/com/example/exceptions/DataRetrieveException.java
@@ -0,0 +1,9 @@
+package com.example.exceptions;
+
+public class DataRetrieveException extends RuntimeException {
+
+    public DataRetrieveException(String message) {
+        super(message);
+    }
+
+}
