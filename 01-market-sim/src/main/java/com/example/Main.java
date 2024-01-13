c56e938a0f0b2939a89ecfe8e6adbb8bde45a9b0
Anthony <anthony.rousseau26@gmail.com>
2024-01-13 15:13:54 +0100

    Adding console navigating fetaure

--- 01-market-sim/src/main/java/com/example/Main.java
+++ 01-market-sim/src/main/java/com/example/Main.java
@@ -5,15 +5,58 @@ package com.example;
 // import org.apache.logging.log4j.LogManager;
 
 import com.example.api.AlphaVantageClient;
+import com.example.model.StockData;
+import com.example.service.StockDataManager;
+import com.example.util.FormatStock;
+
+import java.util.Scanner;
 
 public class Main {
 
     // private static final Logger logger =
     // LogManager.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        StockDataManager manager = new StockDataManager();
         AlphaVantageClient client = new AlphaVantageClient();
-        client.getDailyStockInformation("AAPL");
+        FormatStock fs = new FormatStock();
+
+        while (true) {
+
+            System.out.println("""
+
+                    Menu :
+                        0. Exit
+                        1. Fetch a daily based stock data
+                        2. Display the current stock list
+                            """);
+            String command = scanner.nextLine();
+
+            if ("exit".equalsIgnoreCase(command) || "0".equalsIgnoreCase(command)) {
+                System.out.println("Exiting program.");
+                break;
+            }
+
+            switch (command) {
+                case "1":
+                    System.out.println("Enter stock ticker:");
+                    String ticker = scanner.nextLine();
+                    String s = client.getDailyStockInformation(ticker);
+                    manager.addStockData(fs.FormatStockDaily(s));
+                    System.out.println("Stock Data: " + s);
+                    break;
+
+                case "2":
+                    System.out.println("Current list of stocks : " + manager.getRegisteredTickers());
+
+                default:
+                    System.out.println("Unknown command.");
+                    break;
+            }
+
+        }
 
     }
 }
