a2f27df99d9640e8cb0b8cce65ee7f1017f7d6bb
Anthony <anthony.rousseau26@gmail.com>
2024-01-14 14:22:15 +0100

    Updating model with getters and setters

--- 01-market-sim/src/main/java/com/example/model/DailySerie.java
+++ 01-market-sim/src/main/java/com/example/model/DailySerie.java
@@ -18,4 +18,44 @@ public class DailySerie {
         this.volume = volume;
     }
 
+    public BigDecimal getOpen() {
+        return this.open;
+    }
+
+    public void setOpen(BigDecimal open) {
+        this.open = open;
+    }
+
+    public BigDecimal getHigh() {
+        return this.high;
+    }
+
+    public void setHigh(BigDecimal high) {
+        this.high = high;
+    }
+
+    public BigDecimal getLow() {
+        return this.low;
+    }
+
+    public void setLow(BigDecimal low) {
+        this.low = low;
+    }
+
+    public BigDecimal getClose() {
+        return this.close;
+    }
+
+    public void setClose(BigDecimal close) {
+        this.close = close;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
 }
