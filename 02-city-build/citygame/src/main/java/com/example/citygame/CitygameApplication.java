94bb8dc8412210a81a3bab92da7d3d0bc34ee677
Anthony <anthony.rousseau26@gmail.com>
2024-01-28 14:57:24 +0100

    New project init

---
+++ 02-city-build/citygame/src/main/java/com/example/citygame/CitygameApplication.java
@@ -0,0 +1,13 @@
+package com.example.citygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitygameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitygameApplication.class, args);
+	}
+
+}
---
+++ 02-city-build/citygame/src/test/java/com/example/citygame/CitygameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitygameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
