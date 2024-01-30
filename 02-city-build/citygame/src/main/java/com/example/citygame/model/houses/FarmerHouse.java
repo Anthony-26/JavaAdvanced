4e58be5a5d1a5e3471a79b9c8984e58fa4f0e10c
Anthony <anthony.rousseau26@gmail.com>
2024-01-30 10:45:30 +0100

    Creation of static map for farmer needs and updating the rate

--- 02-city-build/citygame/src/main/java/com/example/citygame/model/houses/FarmerHouse.java
+++ 02-city-build/citygame/src/main/java/com/example/citygame/model/houses/FarmerHouse.java
@@ -1,17 +1,25 @@
 package com.example.citygame.model.houses;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.example.citygame.model.resources.Fish;
 
 public class FarmerHouse extends House {
+    
+    public static final Map<String, Double> resourceNeeds;
+    static {
+        Map<String, Double> aMap = new HashMap<>();
+        aMap.put("Fish", 0.05);
+        resourceNeeds = Collections.unmodifiableMap(aMap);
+    }
+
 
     public FarmerHouse(Fish fish){
         super("Farmer House", 10, 0);
 
-        /* 5 units of fish per minut for a farmer house */
-        getResourceNeeds().put(fish, 5);
-
-        /* Increase the consumption rate according the the needs */
-        fish.increaseConsumptionRate(5);
+        fish.increaseConsumptionRate(0.05d);
     }
 
 }
