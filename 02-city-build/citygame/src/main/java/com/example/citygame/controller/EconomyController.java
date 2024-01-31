e43984cadd6dd0c96c7f05696a2faf8565d28df6
Anthony <anthony.rousseau26@gmail.com>
2024-01-31 19:05:06 +0100

    Building economy controller

--- 02-city-build/citygame/src/main/java/com/example/citygame/controller/EconomyController.java
+++ 02-city-build/citygame/src/main/java/com/example/citygame/controller/EconomyController.java
@@ -1,10 +1,39 @@
 package com.example.citygame.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.citygame.service.BalanceService;
 
 @RestController
-@RequestMapping("/api/balance")
+@RequestMapping("/api/economy")
 public class EconomyController {
+
+    private final BalanceService balanceService;
+
+    @Autowired
+    public EconomyController(BalanceService balanceService) {
+        this.balanceService = balanceService;
+    }
+
+    // @GetMapping("/balance")
+    // public ResponseEntity<Integer> getCurrentBalance() {
+    //     int currentBalance = balanceService.getCurrentBalance();
+    //     return new ResponseEntity<>(currentBalance, HttpStatus.OK);
+    // }
+
+    // @PostMapping("/transaction")
+    // public ResponseEntity<Void> postTransaction(@RequestBody Transaction transaction) {
+    //     boolean success = balanceService.applyTransaction(transaction);
+        
+    //     if (success) {
+    //         return new ResponseEntity<>(HttpStatus.OK);
+    //     } else {
+    //         return new ResponseEntity<>(HttpStatus.CONFLICT);
+    //     }
+    // }
 
 }
