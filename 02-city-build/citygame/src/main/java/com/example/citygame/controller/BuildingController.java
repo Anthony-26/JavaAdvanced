9ae14a3435a1558f0986793866398cea7d8c86d0
Anthony <anthony.rousseau26@gmail.com>
2024-02-03 17:40:28 +0100

    Adding controller method with dynamic request and values

--- 02-city-build/citygame/src/main/java/com/example/citygame/controller/BuildingController.java
+++ 02-city-build/citygame/src/main/java/com/example/citygame/controller/BuildingController.java
@@ -4,9 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.citygame.model.productionbuildings.ProductionBuilding;
 import com.example.citygame.service.BuildingService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/buildings")
@@ -20,6 +24,14 @@ public class BuildingController {
         buildingService.createFishery();
         return new ResponseEntity<>(HttpStatus.OK);
     }
+    
+    @PostMapping("/create")
+    public ResponseEntity<ProductionBuilding> postMethodName(@RequestParam String buildingType) {
+        Class<? extends ProductionBuilding> buildingClass = (Class<? extends ProductionBuilding) Class.forName(buildingType);
+        ProductionBuilding newBuilding = buildingService.createBuilding(buildingClass);
+        return new ResponseEntity<>(newBuilding, HttpStatus.OK);
+    }
+    
     
 
 }
